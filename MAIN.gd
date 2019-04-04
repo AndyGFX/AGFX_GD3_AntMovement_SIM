9fcca1a980d6b04eb7aec8d6a6c614c46d4c0946
AndyGFX <o.kollar@gmail.com>
2019-04-04 19:33:53 +0200

    Updated rule control

--- MAIN.gd
+++ MAIN.gd
@@ -1,7 +1,30 @@
 extends Node2D
 
+var itemList;
+var item_tmp = preload("res://Prefabs/ItemTemplate/RuleItem.tscn")
 func _ready():
+	
+	self.itemList = $Panel/ScrollContainer/ItemList
+	
 	pass
  
 func _process(delta):
 	pass
+
+
+func _on_ButtonBUILD_pressed():
+	pass # Replace with function body.
+
+
+func _on_ButtonSTART_pressed():
+	pass # Replace with function body.
+
+
+func _on_ButtonADD_pressed():
+	var new_rule = item_tmp.instance()
+	get_node("Panel-control/ScrollContainer/ItemList").add_child(new_rule)
+	pass # Replace with function body.
+
+
+func _on_ButtonSTOP_pressed():
+	pass # Replace with function body.
--- Prefabs/ItemTemplate/RuleItem.gd
+++ Prefabs/ItemTemplate/RuleItem.gd
@@ -1,18 +1,56 @@
 extends Panel
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var chk_L;
+var chk_R;
+var chk_U;
+var chk_D;
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.chk_L = get_node("CheckBox-L")
+	self.chk_R = get_node("CheckBox-R")
+	self.chk_U = get_node("CheckBox-U")
+	self.chk_D = get_node("CheckBox-D")
+	self.UncheckCheckbox()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
-
+func UncheckCheckbox():
+	self.chk_D.pressed = false
+	self.chk_U.pressed = false
+	self.chk_L.pressed = false
+	self.chk_R.pressed = false
+	
+	self.chk_D.modulate = Color.black
+	self.chk_U.modulate = Color.black
+	self.chk_L.modulate = Color.black
+	self.chk_R.modulate = Color.black
+	
+	
 func _on_Button_pressed():
-	queue_free()
-	pass # Replace with function body.
+	var items_count = get_parent().get_child_count()
+	if items_count>2:
+		queue_free()
+	pass
+
+
+func _on_CheckBoxL_pressed():
+	self.UncheckCheckbox()
+	self.chk_L.pressed = true
+	self.chk_L.modulate = Color.white
+
+
+func _on_CheckBoxR_pressed():
+	self.UncheckCheckbox()
+	self.chk_R.pressed = true
+	self.chk_R.modulate = Color.white
+
+
+func _on_CheckBoxU_pressed():
+	self.UncheckCheckbox()
+	self.chk_U.pressed = true
+	self.chk_U.modulate = Color.white
+
+
+func _on_CheckBoxD_pressed():
+	self.UncheckCheckbox()
+	self.chk_D.pressed = true
+	self.chk_D.modulate = Color.white
